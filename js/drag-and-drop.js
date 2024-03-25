1e5b6d04e753b54626aa76ec8061f5f4e1176784
jchow7 <164086978+jchow7@users.noreply.github.com>
2024-03-25 03:46:57 -0700

    final project

---
+++ js/drag-and-drop.js
@@ -0,0 +1,28 @@
+let offsetX, offsetY, draggedElement;
+
+document.addEventListener("dragstart", function (event) {
+  draggedElement = event.target;
+  offsetX = event.clientX - draggedElement.getBoundingClientRect().left;
+  offsetY = event.clientY - draggedElement.getBoundingClientRect().top;
+});
+
+document.addEventListener("dragover", function (event) {
+  event.preventDefault();
+});
+
+document.addEventListener("drop", function (event) {
+  event.preventDefault();
+  if (draggedElement) {
+    draggedElement.style.left = event.clientX - offsetX + "px";
+    draggedElement.style.top = event.clientY - offsetY + "px";
+    draggedElement.style.width = "200px"; 
+    draggedElement.style.height = "200px";
+    draggedElement.style.backgroundColor = "transparent";
+  }
+});
+
+document.addEventListener("dragend", function () {
+  draggedElement = null;
+  offsetX = 0;
+  offsetY = 0;
+});
